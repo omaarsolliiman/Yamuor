68b6b868dd8de3182f25a3064bfa6a15be932fd4
Omar Soliman <125391108+omaarsolliiman@users.noreply.github.com>
2023-05-02 13:12:21 +0300

    Add files via upload

---
+++ yamuor-icons.js
@@ -0,0 +1,18 @@
+window.onload = () => { let e = null, t = 0, l = document.querySelector(".landing h1"); clearInterval(e), e = setInterval(() => { l.innerText = l.innerText.split("").map((e, n) => n < t ? l.dataset.value[n] : "ABCDEFGHIJKLMNO"[Math.floor(15 * Math.random())]).join(""), t >= l.dataset.value.length && clearInterval(e), t += 1 / 3 }, 25) }; function sendEmail() { let e = document.getElementById("name").value, t = document.getElementById("email").value, l = document.getElementById("subject").value, n = document.getElementById("message").value; emailjs.send("yamuor", "template_34ievap", { from_name: e, to_name: "omar", from_email: t, subject: l, message: n }).then(e => { console.log("SUCCESS!", e.status, e.text); let t = document.querySelector(".good"); t.classList.add("active"), setTimeout(function () { t.classList.remove("active") }, 3e3) }, e => { console.log("FAILED...", e) }) } const sendBtn = document.getElementById("send"); sendBtn.addEventListener("click", function () { if (document.getElementById("email").value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) && document.getElementById("name").value.match(/^.{3,}\s*$/u) && document.getElementById("subject").value.match(/^\S+$/) && document.getElementById("message").value.match(/^.{20,}\s*$/u)) sendEmail(); else { let e = document.querySelector(".bad"); e.classList.toggle("active"), setTimeout(function () { e.classList.toggle("active") }, 3e3) } }); const divElements = document.querySelectorAll(".icon"); divElements.forEach(e => { e.addEventListener("click", () => { let t = e.querySelector("i").outerHTML, l = document.createElement("input"); l.setAttribute("value", t), document.body.appendChild(l), l.select(), document.execCommand("copy"), document.body.removeChild(l); let n = document.querySelector(".icon-message"); n.classList.add("show"), setTimeout(function () { n.classList.remove("show") }, 1e3) }) });
+let myInput = document.querySelector(".uusge");
+
+function copyToClipboard() {
+    const myInput = document.querySelector(".usage");
+
+    // Select the text in the input element
+    myInput.select();
+
+    // Copy the selected text to the clipboard
+    document.execCommand("copy");
+
+    let n = document.querySelector(".icon-message");
+    n.classList.add("show"),
+    setTimeout(function () { n.classList.remove("show") }, 1e3)
+
+
+}
---
+++ yamuor_icons.dart
@@ -0,0 +1,117 @@
+// Place fonts/yamuor.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: yamuor
+//      fonts:
+//       - asset: fonts/yamuor.ttf
+import 'package:flutter/widgets.dart';
+
+class Yamuor {
+  Yamuor._();
+
+  static const String _fontFamily = 'yamuor';
+
+  static const IconData amazon = IconData(0xea87, fontFamily: _fontFamily);
+  static const IconData brightness_contrast = IconData(0xe9d6, fontFamily: _fontFamily);
+  static const IconData codepen = IconData(0xeae8, fontFamily: _fontFamily);
+  static const IconData css3 = IconData(0xeae6, fontFamily: _fontFamily);
+  static const IconData dropbox = IconData(0xeaae, fontFamily: _fontFamily);
+  static const IconData enter = IconData(0xea13, fontFamily: _fontFamily);
+  static const IconData exit = IconData(0xea14, fontFamily: _fontFamily);
+  static const IconData eye = IconData(0xe9ce, fontFamily: _fontFamily);
+  static const IconData eye_blocked = IconData(0xe9d1, fontFamily: _fontFamily);
+  static const IconData git = IconData(0xeae7, fontFamily: _fontFamily);
+  static const IconData github = IconData(0xeab0, fontFamily: _fontFamily);
+  static const IconData google = IconData(0xea89, fontFamily: _fontFamily);
+  static const IconData heart = IconData(0xe9da, fontFamily: _fontFamily);
+  static const IconData html_five = IconData(0xeae4, fontFamily: _fontFamily);
+  static const IconData html_five2 = IconData(0xeae5, fontFamily: _fontFamily);
+  static const IconData linkedin = IconData(0xeac9, fontFamily: _fontFamily);
+  static const IconData location = IconData(0xe947, fontFamily: _fontFamily);
+  static const IconData location2 = IconData(0xe948, fontFamily: _fontFamily);
+  static const IconData phone = IconData(0xe942, fontFamily: _fontFamily);
+  static const IconData phone_hang_up = IconData(0xe943, fontFamily: _fontFamily);
+  static const IconData globe = IconData(0xe9c9, fontFamily: _fontFamily);
+  static const IconData spotify = IconData(0xea94, fontFamily: _fontFamily);
+  static const IconData telegram = IconData(0xea95, fontFamily: _fontFamily);
+  static const IconData twitter = IconData(0xea96, fontFamily: _fontFamily);
+  static const IconData whatsapp = IconData(0xea93, fontFamily: _fontFamily);
+  static const IconData youtube = IconData(0xea9d, fontFamily: _fontFamily);
+  static const IconData Kaaiioos = IconData(0xe920, fontFamily: _fontFamily);
+  static const IconData ancient = IconData(0xe94c, fontFamily: _fontFamily);
+  static const IconData apeerion = IconData(0xe94b, fontFamily: _fontFamily);
+  static const IconData archivebox = IconData(0xe94a, fontFamily: _fontFamily);
+  static const IconData archivebox_fill = IconData(0xe949, fontFamily: _fontFamily);
+  static const IconData arrow_clockwise = IconData(0xe946, fontFamily: _fontFamily);
+  static const IconData arrow_counterclockwise = IconData(0xe945, fontFamily: _fontFamily);
+  static const IconData automat = IconData(0xe944, fontFamily: _fontFamily);
+  static const IconData back_arrow = IconData(0xe941, fontFamily: _fontFamily);
+  static const IconData bag = IconData(0xe940, fontFamily: _fontFamily);
+  static const IconData bag_fill = IconData(0xe93f, fontFamily: _fontFamily);
+  static const IconData bell = IconData(0xe93e, fontFamily: _fontFamily);
+  static const IconData bell_circle = IconData(0xe93d, fontFamily: _fontFamily);
+  static const IconData bell_circle_fill = IconData(0xe93c, fontFamily: _fontFamily);
+  static const IconData bell_fill = IconData(0xe93b, fontFamily: _fontFamily);
+  static const IconData chat_bubble = IconData(0xe93a, fontFamily: _fontFamily);
+  static const IconData chat_bubble_2 = IconData(0xe939, fontFamily: _fontFamily);
+  static const IconData chat_bubble_2_fill = IconData(0xe938, fontFamily: _fontFamily);
+  static const IconData chat_bubble_fill = IconData(0xe937, fontFamily: _fontFamily);
+  static const IconData chat_bubble_text = IconData(0xe936, fontFamily: _fontFamily);
+  static const IconData chat_bubble_text_fill = IconData(0xe935, fontFamily: _fontFamily);
+  static const IconData checkmark = IconData(0xe934, fontFamily: _fontFamily);
+  static const IconData checkmark_circle = IconData(0xe933, fontFamily: _fontFamily);
+  static const IconData checkmark_circle_fill = IconData(0xe932, fontFamily: _fontFamily);
+  static const IconData checkmark_seal = IconData(0xe930, fontFamily: _fontFamily);
+  static const IconData checkmark_seal_fill = IconData(0xe931, fontFamily: _fontFamily);
+  static const IconData cloud_download = IconData(0xe92f, fontFamily: _fontFamily);
+  static const IconData cloud_download_fill = IconData(0xe92e, fontFamily: _fontFamily);
+  static const IconData coompact = IconData(0xe92d, fontFamily: _fontFamily);
+  static const IconData cube = IconData(0xe92c, fontFamily: _fontFamily);
+  static const IconData deematt = IconData(0xe92b, fontFamily: _fontFamily);
+  static const IconData deeniiz = IconData(0xe92a, fontFamily: _fontFamily);
+  static const IconData eeiiva = IconData(0xe929, fontFamily: _fontFamily);
+  static const IconData ellipsis_circle = IconData(0xe927, fontFamily: _fontFamily);
+  static const IconData ellipsis_circle_fill = IconData(0xe926, fontFamily: _fontFamily);
+  static const IconData ellipsis_vertical_circle = IconData(0xe925, fontFamily: _fontFamily);
+  static const IconData ellipsis_vertical_circle_fill = IconData(0xe928, fontFamily: _fontFamily);
+  static const IconData gear = IconData(0xe924, fontFamily: _fontFamily);
+  static const IconData info = IconData(0xe923, fontFamily: _fontFamily);
+  static const IconData info_circle = IconData(0xe922, fontFamily: _fontFamily);
+  static const IconData info_circle_fill = IconData(0xe921, fontFamily: _fontFamily);
+  static const IconData keep = IconData(0xe91f, fontFamily: _fontFamily);
+  static const IconData logo = IconData(0xe91e, fontFamily: _fontFamily);
+  static const IconData maattbook = IconData(0xe91d, fontFamily: _fontFamily);
+  static const IconData manttek = IconData(0xe91c, fontFamily: _fontFamily);
+  static const IconData matpenter = IconData(0xe91b, fontFamily: _fontFamily);
+  static const IconData mic = IconData(0xe91a, fontFamily: _fontFamily);
+  static const IconData mic_circle = IconData(0xe919, fontFamily: _fontFamily);
+  static const IconData mic_circle_fill = IconData(0xe918, fontFamily: _fontFamily);
+  static const IconData mic_fill = IconData(0xe917, fontFamily: _fontFamily);
+  static const IconData paperclip = IconData(0xe916, fontFamily: _fontFamily);
+  static const IconData pause = IconData(0xe915, fontFamily: _fontFamily);
+  static const IconData pause_circle = IconData(0xe914, fontFamily: _fontFamily);
+  static const IconData pause_circle_fill = IconData(0xe913, fontFamily: _fontFamily);
+  static const IconData pause_fill = IconData(0xe912, fontFamily: _fontFamily);
+  static const IconData pencil = IconData(0xe911, fontFamily: _fontFamily);
+  static const IconData pencil_circle = IconData(0xe910, fontFamily: _fontFamily);
+  static const IconData pencil_circle_fill = IconData(0xe90f, fontFamily: _fontFamily);
+  static const IconData person = IconData(0xe90e, fontFamily: _fontFamily);
+  static const IconData play = IconData(0xe90d, fontFamily: _fontFamily);
+  static const IconData play_circle = IconData(0xe90c, fontFamily: _fontFamily);
+  static const IconData play_circle_fill = IconData(0xe90b, fontFamily: _fontFamily);
+  static const IconData play_fill = IconData(0xe90a, fontFamily: _fontFamily);
+  static const IconData plus = IconData(0xe909, fontFamily: _fontFamily);
+  static const IconData rectangle_paperclip = IconData(0xe908, fontFamily: _fontFamily);
+  static const IconData rectangle_stack = IconData(0xe907, fontFamily: _fontFamily);
+  static const IconData rectangle_stack_fill = IconData(0xe906, fontFamily: _fontFamily);
+  static const IconData search = IconData(0xe905, fontFamily: _fontFamily);
+  static const IconData square_stack = IconData(0xe904, fontFamily: _fontFamily);
+  static const IconData square_stack_fill = IconData(0xe903, fontFamily: _fontFamily);
+  static const IconData trash = IconData(0xe902, fontFamily: _fontFamily);
+  static const IconData trash_circle = IconData(0xe901, fontFamily: _fontFamily);
+  static const IconData trash_circle_fill = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData trash_fill = IconData(0xe94f, fontFamily: _fontFamily);
+  static const IconData viisiion = IconData(0xe94e, fontFamily: _fontFamily);
+  static const IconData yamuor = IconData(0xe94d, fontFamily: _fontFamily);
+}
